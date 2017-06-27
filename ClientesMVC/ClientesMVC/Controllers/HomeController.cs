6097c385098c6fb6da32b0c7e5938342b9735bb0
Manacés Pereira <dev@hkl.com.br>
2017-06-27 16:29:59 -0300

    primeiro commit

---
+++ ClientesMVC/ClientesMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ClientesMVC.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientesMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var urlBase = "https://jsonplaceholder.typicode.com";
+            var client = new RestClient(urlBase);
+            var requisicao = new RestRequest($"/users", Method.GET);
+
+            var resposta = client.Execute<List<Cliente>>(requisicao);
+            var clientes = resposta.Data;
+
+            return View(clientes);
+        }
+    }
+}
